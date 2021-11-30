5cf0ea46d715ae292f0bab489cb95aa331efa61f
Garg <garg882001@gmail.com>
2021-11-30 13:10:53 +0530

    now look java

---
+++ Python/Learning/helo.py
@@ -0,0 +1,7 @@
+import time
+
+# he guys please wait for few seconds , after the four seconds it will start to work
+
+time.sleep(4)
+
+print('helo guys how are you?')  
---
+++ Python/Learning/practice_3.py
@@ -0,0 +1,9 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say(" hello Deepak Garg  ")
+engine.runAndWait()
+
+
+c=4
+d=4
+print(c=d)
